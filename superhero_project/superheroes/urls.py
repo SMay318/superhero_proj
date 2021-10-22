b1b73639777b65e0faf7504795a8d63987cb58c6
Sean May <s_may318@hotmail.com>
2021-10-22 11:02:18 -0400

    added a detail path that shows the details of the superhero, added a create path that allows user to create there own superheroes to the query as well.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/',views.create, name='create')
 
 ]
