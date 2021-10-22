fceb9362a1b701961553285b720b163a985d68db
Sean May <s_may318@hotmail.com>
2021-10-22 15:31:12 -0400

    Added a function to update superheroes database, added a path for the update as well as a template.

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -31,3 +31,19 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def update(request, hero_id):
+    if request.method == "POST":
+        update_hero = Superhero.objects.get(pk=hero_id)
+        update_hero.name = request.POST.get('name')
+        update_hero.alter_ego = request.POST.get('alter_ego')
+        update_hero.primary = request.POST.get('primary')
+        update_hero.secondary = request.POST.get('secondary')
+        update_hero.catchphrase = request.POST.get('catchphrase')
+        return detail(request, hero_id)
+    else:
+        update_hero = Superhero.objects.get(pk=hero_id)
+        context = {
+            'update_hero': update_hero
+        }
+        return render(request, 'superheroes/update.html', context)
